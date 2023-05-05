63e464fbcfd83c02a3064720c54781063af3d54b
zihadx <blankzihad@gmail.com>
2023-05-05 16:08:44 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAu5d9Z0-l0OxO7NliRXiTDky14hvMBj4I",
+  authDomain: "assignment-10-ee8d2.firebaseapp.com",
+  projectId: "assignment-10-ee8d2",
+  storageBucket: "assignment-10-ee8d2.appspot.com",
+  messagingSenderId: "455241285846",
+  appId: "1:455241285846:web:2a8aba56a90aeba8b54977"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Componets/Main.jsx';
+import Contact from './Componets/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
