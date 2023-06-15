796af2359eb3bca25d77b2289d8545c28ccd5c46
zihadx <blankzihad@gmail.com>
2023-06-15 14:55:56 +0600

    create about us section

--- src/Componets/AboutUs.jsx
+++ src/Componets/AboutUs.jsx
@@ -1,20 +1,20 @@
-// import React from "react";
+import React from "react";
 
-// const AboutUs = () => {
-//   return (
-//     <div className="flex flex-col md:flex-row justify-between items-center my-12">
+const AboutUs = () => {
+  return (
+    <div className="flex flex-col md:flex-row justify-between items-center my-12">
       
-//       <div className="w-1/2 flex justify-start">
-//         <img className=" h-[400px] " src="https://i.ibb.co/bH9d06r/pexels-burak-the-weekender-1252841.jpg" alt="" />
-//       </div>
-//       <div className="w-1/2">
-//         <h1 className="font-semibold text-5xl">Daily Special Dishes</h1>
-//         <p className="my-4">Welcome to our restaurant, where culinary excellence meets unparalleled dining experience. We take immense pride in crafting exceptional dishes that tantalize your taste buds and leave you craving for more. At our restaurant, we believe that food is not just sustenance but an art form that ignites passion and creates memories.</p>
-//         <p>Our menu showcases a delightful fusion of flavors, blending traditional recipes with contemporary techniques. Our talented chefs meticulously select the freshest ingredients, sourced locally whenever possible, to ensure the highest quality and to support our community. From farm-fresh vegetables to succulent meats, every dish is a testament to our commitment to providing you with an unforgettable culinary journey.</p>
-//         <button className="btn btn-ghost bg-orange-500 text-white hover:text-black mt-6">Make a reservation</button>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="w-1/2 flex justify-start">
+        <img className=" h-[400px] " src="https://i.ibb.co/bH9d06r/pexels-burak-the-weekender-1252841.jpg" alt="" />
+      </div>
+      <div className="w-1/2">
+        <h1 className="font-semibold text-5xl">Daily Special Dishes</h1>
+        <p className="my-4">Welcome to our restaurant, where culinary excellence meets unparalleled dining experience. We take immense pride in crafting exceptional dishes that tantalize your taste buds and leave you craving for more. At our restaurant, we believe that food is not just sustenance but an art form that ignites passion and creates memories.</p>
+        <p>Our menu showcases a delightful fusion of flavors, blending traditional recipes with contemporary techniques. Our talented chefs meticulously select the freshest ingredients, sourced locally whenever possible, to ensure the highest quality and to support our community. From farm-fresh vegetables to succulent meats, every dish is a testament to our commitment to providing you with an unforgettable culinary journey.</p>
+        <button className="btn btn-ghost bg-orange-500 text-white hover:text-black mt-6">Make a reservation</button>
+      </div>
+    </div>
+  );
+};
 
-// export default AboutUs;
+export default AboutUs;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../../Componets/Hero';
 import Recipes from '../../Componets/Recipes';
 import Chefs from '../../Componets/Chefs';
 import JoinUs from '../../Componets/JoinUs';
-// import AboutUs from '../../Componets/AboutUs';
+import AboutUs from '../../Componets/AboutUs';
 
 const Home = () => {
     return (
@@ -11,7 +11,7 @@ const Home = () => {
            <Hero></Hero>
            <Chefs></Chefs>
            <JoinUs></JoinUs>
-           {/* <AboutUs></AboutUs> */}
+           <AboutUs></AboutUs>
         </div>
     );
 };
